d89e94267d2d4e86857a5177d2a3d752aec02e57
dymitrbaranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-12-08 19:44:26 +0200

    fox2

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -7,6 +7,22 @@ export const Gallery = () => {
       <h1>Gallery</h1>
       <ul>
         <li>
+          <img
+            src="https://i.ibb.co/gbXrNDCR/1-Reflection.png"
+            alt="1 Reflection"
+            border="0"
+          ></img>
+        </li>
+        <li>
+          <img
+            src="https://i.ibb.co/Ld82YHwQ/12.gif"
+            alt="12 бедный"
+            border="0"
+          ></img>
+        </li>
+      </ul>
+      {/* <ul>
+        <li>
           <img src="/public/not-sorted.img/001 Музика Афіша.jpg" alt="" />
         </li>
         <li>
@@ -93,7 +109,50 @@ export const Gallery = () => {
         <li>
           <img src="/public/not-sorted.img/фауст.jpg" alt="" />
         </li>
-      </ul>
+      </ul> */}
     </Main>
   );
 };
+{
+  /* <img src="https://i.ibb.co/gbXrNDCR/1-Reflection.png" alt="1 Reflection" border="0">
+<img src="https://i.ibb.co/60s8x1Gt/2.jpg" alt="2 ачерез культуру" border="0">
+<img src="https://i.ibb.co/QFRFzD3M/2.jpg" alt="2 центр" border="0">
+<img src="https://i.ibb.co/5Wp8b9JK/3.jpg" alt="3 а дима" border="0">
+<img src="https://i.ibb.co/Q3LkFBdL/3.jpg" alt="3 встреча" border="0">
+<img src="https://i.ibb.co/3y9R8BXf/4.jpg" alt="4 после" border="0">
+<img src="https://i.ibb.co/cSwMJczg/5.jpg" alt="5 замок" border="0">
+<img src="https://i.ibb.co/xqdKmD7L/6.jpg" alt="6 дали" border="0">
+<img src="https://i.ibb.co/XxqG8w4Z/7.jpg" alt="7 Христос и Самаритянка" border="0">
+<img src="https://i.ibb.co/NdfC3cvc/8.jpg" alt="8 палац" border="0">
+<img src="https://i.ibb.co/d1HrjQX/8.jpg" alt="8 аференц" border="0">
+<img src="https://i.ibb.co/b9mdrFd/9.jpg" alt="9 дюрер" border="0">
+<img src="https://i.ibb.co/Vp0p0nbD/10.jpg" alt="10 каменец" border="0">
+<img src="https://i.ibb.co/S4pC9w8S/10.jpg" alt="10 меджибож" border="0">
+<img src="https://i.ibb.co/N2nJMfGq/11.jpg" alt="11 фототеатр" border="0">
+<img src="https://i.ibb.co/MxM386Lk/12.jpg" alt="12 арыцарь бедный" border="0">
+<img src="https://i.ibb.co/Ld82YHwQ/12.gif" alt="12 бедный" border="0">
+<img src="https://i.ibb.co/CKvqG31S/13.jpg" alt="13 виденье" border="0">
+<img src="https://i.ibb.co/hJSVyMT8/13.jpg" alt="13 испания" border="0">
+<img src="https://i.ibb.co/HTtR0zS7/14.jpg" alt="14 самчики" border="0">
+<img src="https://i.ibb.co/mVr1WQp7/15.jpg" alt="15 романтика" border="0">
+<img src="https://i.ibb.co/gBG6ZXY/026.jpg" alt="026 в" border="0">
+<img src="https://i.ibb.co/vCLQxszH/027.jpg" alt="027" border="0">
+<img src="https://i.ibb.co/ds1JrT0v/center-2015-1.jpg" alt="center 2015 1" border="0">
+<img src="https://i.ibb.co/KpctBdMj/center-2015-2.jpg" alt="center 2015 2" border="0">
+<img src="https://i.ibb.co/Qj7CJWjJ/DSC-0018.jpg" alt="DSC 0018" border="0">
+<img src="https://i.ibb.co/JFFMTdpP/DSC-0483.jpg" alt="DSC 0483" border="0">
+<img src="https://i.ibb.co/3mkpq7Qn/DSC-0748.jpg" alt="DSC 0748" border="0">
+<img src="https://i.ibb.co/dwcmTGk0/DSC-1625.jpg" alt="DSC 1625" border="0">
+<img src="https://i.ibb.co/d0NrNyRb/DSC-2254.jpg" alt="DSC 2254" border="0">
+<img src="https://i.ibb.co/twbyRygH/P4270408.jpg" alt="P4270408" border="0">
+<img src="https://i.ibb.co/KTzzP38/image.jpg" alt="асамблея" border="0">
+<img src="https://i.ibb.co/xKJKP8J8/image.jpg" alt="выставка" border="0">
+<img src="https://i.ibb.co/F47bcX0s/image.jpg" alt="денис" border="0">
+<img src="https://i.ibb.co/7d52vftx/2.jpg" alt="денис2" border="0">
+<img src="https://i.ibb.co/LzHPPnN9/1.jpg" alt="Лист 1" border="0">
+<img src="https://i.ibb.co/N6KMncZv/image.jpg" alt="монастырский" border="0">
+<img src="https://i.ibb.co/7tP3P90j/image.jpg" alt="после встречи" border="0">
+<img src="https://i.ibb.co/CpsQ2QwN/image.jpg" alt="центр" border="0">
+<img src="https://i.ibb.co/PzVbCL0d/1.jpg" alt="Чарівне середньовіччя 1" border="0">
+<img src="https://i.ibb.co/MDr1j5H6/image.jpg" alt="я и дима" border="0"></img> */
+}
